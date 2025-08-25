8fc5df7f68b495cb7671c38c5f271caebd6a5179
Dimitrios <115547071+dimitrilahanas@users.noreply.github.com>
2025-08-25 12:53:33 +1000

    Task Setters

--- src/Task.java
+++ src/Task.java
@@ -16,6 +16,48 @@ public class Task {
         this.taskStatus         = taskStatus;
     }
 
-    
+    public void setTaskName() {
+        System.out.print("Task Name: ");
+        taskName = input.nextLine();
+    }
+
+    public void setTaskDate(int Year, int Month, int Day) {
+        taskDate = Calendar.getInstance();
+        System.out.println("Enter the date it is due (number format):");
+
+        System.out.print("Day: ");
+        Day = input.nextInt();
+
+        System.out.print("Month: ");
+        Month = input.nextInt();
+
+        System.out.print("Year: ");
+        Year = input.nextInt();
+
+        taskDate.set(Year, Month, Day);
+    }
+
+    public void setTaskDescription() {
+        System.out.print("Task Description: ");
+        taskDescription = input.nextLine();
+    }
+
+    public void setTaskStatus() {
+        System.out.print("Task Completed (Yes/No): ");
+        String status = input.next().toLowerCase();
+
+        while (true) {
+            switch (status) {
+            case "yes":
+                taskStatus = true;
+                break;
+            case "no":
+                taskStatus = false;
+                break;
+            default:
+                System.out.print("Invalid Input.");
+            }
+        }
+    }
 
 }
