a70c9be1dbdfa30fa041edebeb2cb9d5852af401
Dimitrios <115547071+dimitrilahanas@users.noreply.github.com>
2025-08-25 12:29:25 +1000

    Task Constructor

--- src/Task.java
+++ src/Task.java
@@ -1,3 +1,21 @@
+import java.util.Calendar;
+import java.util.Scanner;
+
 public class Task {
+    private String taskName;
+    private Calendar taskDate;
+    private String taskDescription;
+    private boolean taskStatus;
+
+    Scanner input = new Scanner(System.in);
+
+    public Task(String taskName, Calendar taskDate, String taskDescription, boolean taskStatus) {
+        this.taskName           = taskName;
+        this.taskDate           = taskDate;
+        this.taskDescription    = taskDescription;
+        this.taskStatus         = taskStatus;
+    }
+
     
+
 }
